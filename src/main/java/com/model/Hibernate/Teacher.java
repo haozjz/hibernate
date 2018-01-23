eec202e7548a61ada2828d12140b337c712b7559
hszjadmin <hszjadmin@WINDOWS-SYS.hs.handsome.com.cn>
2018-01-23 17:32:32 +0800

    1

--- src/main/java/com/model/Hibernate/Teacher.java
+++ src/main/java/com/model/Hibernate/Teacher.java
@@ -1,5 +1,6 @@
 package com.model.Hibernate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -12,7 +13,14 @@ public class Teacher {
 	private int id;
 	private String name;
 	private String title;
+	private String ownerName; //不参与持久化，数据库不可见
 	
+	public String getOwnerName() {
+		return ownerName;
+	}
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
 	/**
 	 * id注解表示主键，可以放在成员变量上面，
 	 * 不一定要放在get方法上面，但是会破坏封装性，hibernat可以访问私有的变量上
@@ -40,6 +48,8 @@ public class Teacher {
 		this.name = name;
 	}
 
+///当字段名和表中不一致时需要指定column值 
+	@Column(name="_title")
 	public String getTitle() {
 		return title;
 	}
